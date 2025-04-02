2028eda3b534002ada488bebfb59ee946fdbfe62
Eric <ericgeorge4477@gmail.com>
2025-04-02 15:13:38 +0200

    feat: add new user to database

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { createClient } from "@/auth/server";
+import { prisma } from "@/db/prisma";
 import { handleError } from "@/lib/utils";
 
 export const loginUserAction = async (email: string, password: string) => {
@@ -43,6 +44,13 @@ export const signUpUserAction = async (email: string, password: string) => {
     if (!userId) throw new Error("Error signing up");
 
     // add user to database
+
+    await prisma.user.create({
+      data: {
+        id: userId,
+        email,
+      },
+    });
 
     return { errorMessage: null };
   } catch (error) {
