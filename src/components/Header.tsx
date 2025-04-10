13a8d9f098ff240df25f851c01a67ff9264ff113
Eric <ericgeorge4477@gmail.com>
2025-04-10 15:20:04 +0200

    feat: add SidebarTrigger component from ShadCN

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,7 @@ import { Button } from "./ui/button";
 import DarkModeToggle from "./DarkModeToggle";
 import LogoutButton from "./LogoutButton";
 import { getUser } from "@/auth/server";
+import { SidebarTrigger } from "./ui/sidebar";
 
 async function Header() {
   const user = await getUser();
@@ -14,6 +15,7 @@ async function Header() {
       className="relative flex h-24 px-3 w-full items-center justify-between bg-popover sm:px-8"
       style={{ boxShadow: shadow }}
     >
+      <SidebarTrigger className="absolute left-1 top-1" />
       <Link className="flex items-center gap-2" href="/">
         <Image
           priority
